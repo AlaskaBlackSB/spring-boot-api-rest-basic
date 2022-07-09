f419d503679b99f38169b217ff7fae2e9c257a7e
Agner Alexis Plascencia Amador <agner1803@hotmail.com>
2022-07-08 19:33:03 -0500

    chore: inicio del proyecto

---
+++ src/main/java/com/kosmos/consultorio/springbootconsultorio/SpringBootConsultorioApplication.java
@@ -0,0 +1,13 @@
+package com.kosmos.consultorio.springbootconsultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsultorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosmos/consultorio/springbootconsultorio/SpringBootConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos.consultorio.springbootconsultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
